f9e563d819a9c87ca991179adab08027dd3e756b
luca <lucasanatagostino25@gmail.com>
2016-05-12 11:26:07 +0200

    primocommit

---
+++ src/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('UserBundle:Default:index.html.twig');
+    }
+
+
+        public function aboutAction()
+    {
+        return $this->render('UserBundle:Default:about.html.twig');
+    }
+}
